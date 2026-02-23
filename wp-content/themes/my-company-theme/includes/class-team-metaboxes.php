e9272e099c83505508ab43a9a3bfd0b27c7ff5d2
Osama Alasmi <153827554+osama0210@users.noreply.github.com>
2026-02-23 09:50:20 +0100

    Added: Basic front-page template and complete team meta-boxes with nonce and save logic

--- wp-content/themes/my-company-theme/includes/class-team-metaboxes.php
+++ wp-content/themes/my-company-theme/includes/class-team-metaboxes.php
@@ -24,6 +24,8 @@ class Team_Meta
         $email = get_post_meta($post->ID, '_team_email', true);
         $linkedin = get_post_meta($post->ID, '_team_linkedin', true);
 
+        wp_nonce_field('team_meta_box', 'team_meta_box_nonce');
+
         ?>
         <label><?php _e('Function', 'my-company-theme'); ?></label>
         <input type="text" name="_team_function" value="<?php echo esc_attr($function); ?>">
@@ -36,8 +38,27 @@ class Team_Meta
         <?php
     }
 
-    public function save_meta_box()
+    public function save_meta_box($post_id)
     {
+        if (get_post_type($post_id) !== 'team') {
+            return;
+        }
+
+        if (!isset($_POST['team_meta_box_nonce']) || !wp_verify_nonce($_POST['team_meta_box_nonce'], 'team_meta_box')) {
+            return;
+        }
+
+        if (isset($_POST['_team_function'])) {
+            update_post_meta($post_id, '_team_function', sanitize_text_field($_POST['_team_function']));
+        }
+
+        if (isset($_POST['_team_email'])) {
+            update_post_meta($post_id, '_team_email', sanitize_email($_POST['_team_email']));
+        }
+
+        if (isset($_POST['_team_linkedin'])) {
+            update_post_meta($post_id, '_team_linkedin', esc_url_raw($_POST['_team_linkedin']));
+        }
 
     }
 }
